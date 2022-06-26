01fca1bac22b091501b68bab47d6198e6144bb0b
ecikou <747148841@qq.com>
2022-06-26 18:04:36 +0800

    完成登录功能

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: {}
   },
   getters: {
   },
   mutations: {
+    setUser (state, paylod) {
+      state.user = paylod
+    }
   },
   actions: {
   },
   modules: {
-  }
+  },
+  plugins: [vuexLocal.plugin]
 })
